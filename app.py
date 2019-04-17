b8a2491c93cd1274fc21021f0fb692241da65a1c
HoveringHalibut <rich@ericksons.us>
2019-04-16 21:19:53 -0500

    Fix method name

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def timeLapse():
 
 def gen(camera):
   while True:
-    frame = camera._get_frame()
+    frame = camera.get_frame()
     yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
